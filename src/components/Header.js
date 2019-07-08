c37add1f8b25fd61adc1b1398011390184adbc84
John Thomson <john.thomson@fanduel.com>
2019-07-08 17:05:37 +0100

    add styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = (props) => {
     const { image, name, signOut } = props
     return (
         <Wrapper>
-            <header className="App-header">
+            <header>
               <Heading>Hey {name}</Heading>
               <SubHeading>this is your to-do list</SubHeading>
             </header>
